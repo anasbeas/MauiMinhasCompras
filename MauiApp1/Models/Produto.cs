97bd96e60c7967c3b7dd579074e5b25c4fe9f74b
anabe <anabe@DESKTOP-FT5T5EA>
2026-03-01 16:08:52 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp1/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MauiApp1.Models
+{
+    internal class Produto
+    {
+    }
+}
